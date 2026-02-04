3ec0ddb51c67df369207429b8640bbc345d22ef9
João Willian Belarmino Spejo <joaowillian.spejo@outlook.com>
2026-02-04 14:17:41 -0300

    add dont duplicate movies test

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -25,6 +25,38 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
   await page.toast.haveText('UhullCadastro realizado com sucesso!')
 })
 
+test('não deve poder cadastrar um filme duplicado', async ({ page }) => {
+  const randomMovie = Math.floor(Math.random() * data.movies.length)
+
+  const movie = data.movies[randomMovie]
+
+  await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}'`)
+
+  await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+
+  await page.movies.create(
+    movie.title,
+    movie.overview,
+    movie.company,
+    movie.release_year,
+    movie.cover,
+    movie.featured
+  )
+
+  await page.toast.haveText('UhullCadastro realizado com sucesso!')
+
+  await page.movies.create(
+    movie.title,
+    movie.overview,
+    movie.company,
+    movie.release_year,
+    movie.cover,
+    movie.featured
+  )
+
+  await page.toast.haveText('Oops!Este conteúdo já encontra-se cadastrado no catálogo')
+})
+
 test('não deve cadastrar um filme sem preencher os campos obrigatórios', async ({ page }) => {
   await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
 
